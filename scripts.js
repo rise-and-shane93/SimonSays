3a107ded7d4e3e8fb6e8378930841c6681144d00
rise_and_shane93 <cdtharper@gmail.com>
2018-10-22 06:36:58 -0400

    10/21 commit

--- scripts.js
+++ scripts.js
@@ -2,86 +2,130 @@
 
 var colors = ['green', 'red', 'yellow', 'blue'];
 var gameColors = [];
+var timeInterval = 333;
+
+/*sequenceLength is used to limit the number of items in the random colors
+array that the function can produce.*/
+var sequenceLength = 5;
+
+/*sequenceLengthInv is the "opposite" of sequenceLength in the sense
+that the length of the random color array minus this number equals
+the sequence length*/
+var sequenceLengthInv = 5;
+var counter = 0;
 
 //generate random sequence of colors and push to gameColors array
-for (var i = 0; i < 2; i++) {
+for (var i = 0; i < 10; i++) {
     var randomColor = Math.floor(Math.random() * colors.length);
     gameColors.push(colors[randomColor]);
 }
 
-console.log(gameColors);
-
+//select the color divs from the HTML page
 var green = document.querySelector('.green');
 var red = document.querySelector('.red');
 var yellow = document.querySelector('.yellow');
 var blue = document.querySelector('.blue');
+//this selects all the colors
+var color = document.querySelectorAll('.color');
 
-function colorSequenceOn() {
-    if (gameColors[i] === 'green') {
-        setTimeout(function() {
+//change color on click
+for (var i = 0; i < color.length; i++) {
+    color[i].addEventListener('click', function(e) {
+        var fourColors = this.getAttribute('class');
+        if (fourColors.includes('green')) {
             green.style.backgroundColor = '#00ff00';
-        },timeInterval)
-        setTimeout(function() {
-            green.style.backgroundColor = '#006400';
-        },1000);
-    } else if (gameColors[i] === 'yellow') {
-        setTimeout(function() {
+            setTimeout(function () {
+                green.style.backgroundColor = '#006400';
+            }, 125);
+        } else if (fourColors.includes('yellow')) {
             yellow.style.backgroundColor = 'yellow';
-        },timeInterval);
-        setTimeout(function() {
-            yellow.style.backgroundColor = '#999900';
-        },1000);
-    } else if (gameColors[i] === 'red') {
-        setTimeout(function() {
+            setTimeout(function () {
+                yellow.style.backgroundColor = '#999900';
+            }, 125);
+        } else if (fourColors.includes('red')) {
             red.style.backgroundColor = '#ff0000';
-        }, timeInterval);
-        setTimeout(function() {
-            red.style.backgroundColor = '#B22222';
-        },1000);
-    } else {
-        setTimeout(function() {
+            setTimeout(function () {
+                red.style.backgroundColor = '#B22222';
+            }, 125);
+        } else if (fourColors.includes('blue')) {
             blue.style.backgroundColor = '#0000ff';
-        },timeInterval);
-        setTimeout(function() {
-            blue.style.backgroundColor = '#000080';
-        },1000);
+            setTimeout(function () {
+                blue.style.backgroundColor = '#000080';
+            }, 125);
+        }
+    });
+}
+//find a way to refactor this, it's fairly redundant
+
+console.log(gameColors);
+//function to turn on the colors in the random sequence
+function colorSequenceOn(i) {
+    if (gameColors[i] === 'green') {
+        green.style.backgroundColor = '#00ff00';
+    } else if (gameColors[i] === 'yellow') {
+        yellow.style.backgroundColor = 'yellow';
+    } else if (gameColors[i] === 'red') {
+        red.style.backgroundColor = '#ff0000';
+    } else if (gameColors[i] === 'blue')  {
+        blue.style.backgroundColor = '#0000ff';
+    } else {
+        console.log('colorSequenceOn is broken.');
     }
-    timeInterval += 1000;
 };
 
-function colorSequenceOff() {
+//function to turn off the colors in the random sequence
+function colorSequenceOff(i) {
     if (gameColors[i] === 'green') {
         green.style.backgroundColor = '#006400';
     } else if (gameColors[i] === 'yellow') {
         yellow.style.backgroundColor = '#999900';
     } else if (gameColors[i] === 'red') {
         red.style.backgroundColor = '#B22222';
-    } else {
+    } else if (gameColors[i] === 'blue') {
         blue.style.backgroundColor = '#000080';
     }
-    /*
-    if (gameColors[i] === 'blue') {
-        setTimeout(function() {
-            green.style.backgroundColor = '#006400';
-        }, 1000);
-    } else if (gameColors[i] === 'yellow') {
-        setTimeout(function() {
-            yellow.style.backgroundColor = '#999900';
-        }, 1000);
-    } else if (gameColors[i] === 'red') {
-        setTimeout(function() {
-            red.style.backgroundColor = '#B22222';
-        }, 1000);
-    } else {
-        setTimeout(function() {
-            blue.style.backgroundColor = '#000080';
-        }, 1000);
-    } */
+};
+
+//function for the game to play the random sequence of colors
+function randomSequence() {
+    setTimeout(colorSequenceOn, timeInterval, i);
+    setTimeout(colorSequenceOff, timeInterval + 333, i);
+    timeInterval += 666;
 }
-var timeInterval = 1000;
+
+//function declaration that listens for user's input
+function userInput() {
+    color[i].addEventListener('click', function(e) {
+        var result = this.getAttribute('class');
+        if (result.includes(gameColors[counter]) && counter < sequenceLength) {
+            console.log('correct');
+            counter++;
+        } else {
+            alert('incorrect! You have lost the game!');
+        }
+    });
+}
+
+if (sequenceLengthInv >= 0) {
+    // have the game play the random sequence of colors
+    for (var i = 0; i < gameColors.length - sequenceLengthInv; i++) {
+        randomSequence();
+    }
+
+    //listens for the user's input
+    for (var i = 0; i < color.length; i++) {
+        userInput();
+    }
+    sequenceLength++;
+    sequenceLengthInv--;
+}
+
 // have the game play the random sequence of colors
-for (var i = 0; i < gameColors.length; i++) {
-    colorSequenceOn();
-    // setTimeout(colorSequenceOff, timeInterval);
-    // timeInterval += 1000;
-}
+// for (var i = 0; i < gameColors.length - sequenceLength; i++) {
+//     randomSequence();
+// }
+
+//listens for the user's input
+// for (var i = 0; i < color.length; i++) {
+//     userInput();
+// }
